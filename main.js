cf3ab72830468fb7562c52d47cef5c99fb3bfb60
kamalus <formation.kamal.haddou@gmail.com>
2022-04-13 11:39:10 +0200

    maj

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-// Ecrire OK dans la console.
-console.log("ok");
+"use strict"
+// Déclarer des variables
+let n = 5;
+//Déclaration multiples
+let a = 1,
+    b = 2,
+    c
+// camelCase
+let tauxDeTVA = 0.20;
+tauxDeTVA = "20%"
+// Littéraux numériques
+let x = -123.456;
+x = 12e3 // 12000 12 x 10puissance3
+x = 0x7B; // 11 + 7*16 = 123 (en hexadecimal, il faut commencer par 0x)
+// Littéraux chaînes
+let s = "Toto";
+s = `j'aime "mon" toto`;
+console.log(`La TVA est à ${tauxDeTVA}`)
